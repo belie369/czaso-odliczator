ae9b146fc063527037947746d176a20606ac47f8
belie369 <katarzyna.derbich@gmail.com>
2022-05-24 12:48:33 +0200

    Added a JavaScript file

---
+++ script.js
@@ -0,0 +1,49 @@
+const setting = document.querySelector('.settings');
+const settingsBtn = document.querySelector('.settings-btn');
+const imageSection = document.querySelector('.image-section');
+
+const eventName = document.querySelector('#event-name');
+const eventDay = document.querySelector('#event-day');
+const eventMonth = document.querySelector('#event-month');
+const eventYear = document.querySelector('#event-year');
+const eventImg = document.querySelector('#event-image');
+
+const daysCount = document.querySelector('.days-count');
+const hoursCount = document.querySelector('.hours-count');
+const minutesCount = document.querySelector('.minutes-count');
+const secondsCount = document.querySelector('.seconds-count');
+
+const saveBtn = document.querySelector('.save');
+const eventSpan = document.querySelector('.event');
+let usersTime;
+
+const setTime = () => {
+	const currentTime = new Date();
+	const result = usersTime - currentTime;
+
+	const days = Math.floor(result / 1000 / 60 / 60 / 24);
+	const hours = Math.floor(result / 1000 / 60 / 60) % 24;
+	const minutes = Math.floor(result / 1000 / 60) % 60;
+	const seconds = Math.floor(result / 1000) % 60;
+
+	daysCount.textContent = days;
+	hoursCount.textContent = hours;
+	minutesCount.textContent = minutes;
+	secondsCount.textContent = seconds;
+};
+
+// https://cdn.pixabay.com/photo/2022/04/26/20/37/coffee-7159137_960_720.jpg
+
+const appUpdate = () => {
+	eventSpan.textContent = eventName.value;
+	usersTime = new Date(`${eventMonth.value} ${eventDay.value} ${eventYear.value}`);
+	imageSection.style.backgroundImage = `url('${eventImg.value}')`;
+	setTime();
+};
+
+settingsBtn.addEventListener('click', () => {
+	setting.classList.toggle('active');
+});
+saveBtn.addEventListener('click', appUpdate);
+appUpdate();
+setInterval(setTime, 1000);
